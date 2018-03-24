8df2ba0eb83ecb9ae1db5f2c212722ce08660b73
profesor741 <36328898+profesor741@users.noreply.github.com>
2018-03-24 12:35:26 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(".sqrmain").mouseenter( function (){
+	$(".circle").css("visibility", "visible");
+}
+);
+
+$(".sqrroutete").mouseenter( function (){
+	$(".sqrroutete").addClass("loader");
+});
+
+$(".sqrroutete").mouseenter( function (){
+	$(".circle").animate({marginLeft: "0px"})
+});
